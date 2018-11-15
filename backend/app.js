b20b91743399673ca58386038202db07b7dcf467
powers_rc <52507cp$>
2018-11-15 08:28:48 -0700

    Preparing for PROD

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const path = require('path');
+
 
 // Set up the express app
 const app = express();
+
+//require env vars
+require('dotenv').config()
 
 // Log requests to the console.
 app.use(logger('dev'));
@@ -12,10 +17,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../blog/build')));
+
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 require('./server/routes')(app);
-app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the beginning of nothingness.',
-}));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../blog/build/index.html'));
+});
 
 module.exports = app;

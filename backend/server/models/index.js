1c8d1a57d86d3afed77f0d941442bd52f18634ec
powers_rc <52507cp$>
2018-12-04 15:27:06 -0700

    create sequelize instance based on env

--- backend/server/models/index.js
+++ backend/server/models/index.js
@@ -7,7 +7,7 @@ const db = {};
 let sequelize;
 console.log(process.env.NODE_ENV)
   if(process.env.NODE_ENV !== 'development'){
-    sequelize = new Sequelize(process.env.DATABASE_URL, {host:process.env.HOST, dialect:process.env.DIALECT});
+    sequelize = new Sequelize(process.env.DATABASE_URL, {host:process.env.HOST, dialect:process.env.DIALECT, dialectOptions: {ssl: true}});
   }
   else{
     sequelize = new Sequelize(process.env.DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {host:process.env.HOST, dialect:process.env.DIALECT});

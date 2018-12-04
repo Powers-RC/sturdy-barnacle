f8d612f275b8b711b55d664156ff5be5c905fd45
powers_rc <52507cp$>
2018-12-03 20:08:47 -0700

    added different sequelize instances based on env

--- backend/server/models/index.js
+++ backend/server/models/index.js
@@ -7,14 +7,13 @@ const basename = path.basename(module.filename);
 const db = {};
 
 let sequelize;
-//if (config.use_env_variable) {
   console.log(process.env)
-  sequelize = new Sequelize(process.env.DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {host:process.env.HOST, dialect:process.env.DIALECT});
-//} else {
-//  sequelize = new Sequelize(
-//    config.database, config.username, config.password, config
-//  );
-//}
+  if(process.env !== 'Development'){
+    sequelize = new Sequelize(process.env.DATABASE_URL, {host:process.env.HOST, dialect:process.env.DIALECT});
+  }
+  else{
+    sequelize = new Sequelize(process.env.DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {host:process.env.HOST, dialect:process.env.DIALECT});
+  }
 
 fs
   .readdirSync(__dirname)

936703fee915db4476ae0d853cecdd818831c9e2
powers_rc <52507cp$>
2018-12-05 22:09:52 -0700

    Updated sequelize production instance

--- backend/server/models/index.js
+++ backend/server/models/index.js
@@ -13,7 +13,7 @@ console.log(`Current Running Environment: ${process.env.NODE_ENV}`)
     sequelize = new Sequelize(process.env.DATABASE_URL, {host:process.env.HOST, dialect:process.env.DIALECT, dialectOptions: {ssl: true}});
   }
   else{
-    sequelize = new Sequelize(process.env.DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {host:process.env.HOST, dialect:process.env.DIALECT});
+    sequelize = new Sequelize(process.env.DATABASE_URL, {host:process.env.HOST, dialect:process.env.DIALECT, dialectOptions: {ssl: true}});
   }
 
 fs

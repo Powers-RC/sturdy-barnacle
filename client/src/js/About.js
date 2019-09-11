60f24c1fdd53e5fa514c6d3842c88bb4f8670100
powers_rc <52507cp$>
2019-09-10 22:41:44 -0600

    Added Features To About Section, Title, Image, Testing Floating Text

--- client/src/js/About.js
+++ client/src/js/About.js
@@ -1,13 +1,23 @@
 import React, {Component} from "react";
-const fishIcon = require("..//static/icons/fish.png");
+const fishIcon = require("../static/icons/fish.png");
+const portrait = require("../static/photos/portrait.jpg");
 
 export class About extends Component{
 
   render(){
     return(
       <div id="about-section" className="About-Section">
-        <img  src={fishIcon} alt="Fish Icon" height="50" width="50"/>
-        <p>TESTING!!!!</p>
+        <div className="about-title">
+            <h2>About</h2>
+            <img  src={fishIcon} alt="Fish Icon" height="50" width="50"/>
+        </div>
+        <div className="portrait-block">
+            <p>
+                <img  src={portrait} alt="Self Portrait of Author" height="300" width="400"/>
+                Testing to see if the text I am writing will float around my portrait in the about section of my webpagae.
+                <br/>
+            </p>
+        </div>
       </div>
     );
   }

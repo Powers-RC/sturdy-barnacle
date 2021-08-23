d875c662c63b8fe4b0e06937a1ff97c44edbf447
powers_rc <powerscameron@ymail.com>
2021-08-22 22:34:43 -0600

    Forgot I Removed The Ignore
    Also mode site root project root, rather than having it nested

---
+++ src/js/slides/PersonalWebSite.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {Slide} from 'pure-react-carousel'
+
+const githubIcon = require("../../static/icons/github.png");
+const personWebPagePhoto= require("../../static/photos/personalWebPage2.png");
+
+
+export class PersonWebSiteSlide extends React.Component{
+
+    render(){
+        return(
+            <Slide index={0}>
+                <div className="project-container">
+                    <div className="project-title-container">
+                        <h3 className="project-title">Project Name</h3>
+                        <h2>Strudy Barnicle</h2>
+                    </div>
+                    <div className="project-details">
+                        <div className="project-description">
+                            <p>
+                                Most of my work thus far has been strictly employer directed, 
+                                so I made it a goal to change that and start creating tools and apps that I design 
+                                from inspiration to creation. This site is the first step in displaying the works to
+                                come.
+                            </p>
+                            <h3>Tools</h3>
+                            <ul className="project-tech-stack">
+                                <li>HTML</li>
+                                <li>CSS</li>
+                                <li>React.js</li>
+                            </ul>
+                            <div className="project-github-link">
+                                <a href="https://github.com/Powers-RC/sturdy-barnacle">
+                                    <img src={githubIcon} alt="Github Link To Personal Project" height="10%" width="10%"/>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="project-depiction">
+                            <img className="project-picture" src={personWebPagePhoto} alt="Personal site project"/>
+                        </div>
+                    </div>
+                </div>
+            </Slide>
+        );
+    }
+}
